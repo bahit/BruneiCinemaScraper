d1269816ba2160f53c0fdbf8bdf387669b63d5b6
Bahit Hamid <bahit.hamid@gmail.com>
2014-10-01 18:13:35 +0800

    Added parsing The mall cineplex.

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var url = 'http://timescineplex.com/schedule/';
+// var url = 'http://timescineplex.com/schedule/';
+var url1 = 'http://timescineplex.com/empire/schedule/';
 
-request(url, function(err, resp, body){
+
+request(url1, function(err, resp, body){
+  console.log('** Getting data from Times Cineplex.. **');
+  
   if (err) {
     throw err;
   }
@@ -62,5 +66,63 @@ request(url, function(err, resp, body){
       console.log('Date: ' + date);
       console.log('Time: ' + movTime);
     });
+    
   });
+  console.log('** Finished processing Times Cineplex. **');
+});
+
+
+
+var url2 = 'http://www.mall-ticket.com/visShowtimes.aspx';
+
+request(url2, function(err, resp, body){
+  console.log('** Getting data from The Mall Cineplex.. **');
+  
+  if (err) {
+    throw err;
+  }
+  $ = cheerio.load(body);
+  $('.ShowtimesSummaryRow').each(function() {
+    $(this).find('.ShowtimesMovieLink').each(function() {
+      console.log('Title: ' + $(this).text());
+    });
+    // $(this).find('.movie-poster').each(function() {
+    //   posterURL = $(this).find('a').attr('href');
+    //   console.log('Poster: ' + posterURL);
+    // });
+    // $(this).find('.movie-poster').each(function() {
+    //   miniPosterURL = $(this).find('img').attr('src');
+    //   console.log('Poster: ' + miniPosterURL);
+    // });
+    $('.ShowtimesFilterDropDownList').each(function() {
+      console.log('Date: ' + $(this).text().trim());
+    });
+    console.log('Time: ');
+    $(this).find('.ShowtimesSessionLink').each(function() {
+      console.log('Time: ' + $(this).text());
+    });
+  });
+  $('.ShowtimesSummaryRowAlt').each(function() {
+    $(this).find('.ShowtimesMovieLink').each(function() {
+      console.log('Title: ' + $(this).text());
+    });
+    // $(this).find('.movie-poster').each(function() {
+    //   posterURL = $(this).find('a').attr('href');
+    //   console.log('Poster: ' + posterURL);
+    // });
+    // $(this).find('.movie-poster').each(function() {
+    //   miniPosterURL = $(this).find('img').attr('src');
+    //   console.log('Poster: ' + miniPosterURL);
+    // });
+    $('.ShowtimesFilterDropDownList').each(function() {
+      console.log('Date: ' + $(this).text().trim());
+    });
+    console.log('Time: ');
+    $(this).find('.ShowtimesSessionLink').each(function() {
+      console.log($(this).text());
+    });
+  });
+
+  console.log('** Finished processing The Mall Cineplex. **');
+
 });
